d6cdc507b9a110244f3761872e2d47607399d236
Matthew Bodily <matthewbodily@Matthews-MacBook-Air.local>
2019-02-12 13:31:33 -0700

    Added routes.js, and set up the routing structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { HashRouter } from 'react-router-dom';
+import routes from './routes';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <HashRouter>
+        <div className="App">
+          {routes}
+        </div>
+      </HashRouter>
     );
   }
 }
---
+++ src/routes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
+import RegisterStudent from './components/Register/RegisterStudent';
+import RegisterTutor from './components/Register/RegisterTutor';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Subjects from './components/Subjects/Subjects';
+import TutorList from './components/TutorList/TutorList';
+import TutorView from './components/TutorView/TutorView';
+import StudentProfile from './components/Profiles/StudentProfile';
+import TutorProfile from './components/Profiles/TutorProfile';
+import StudentList from './components/StudentList/StudentList';
+import MyTutors from './components/MyTutors/MyTutors';
+
+export default (
+    <Switch>
+        <Route exact path = '/' component = {Login}/>
+        <Route path = '/registerstudent' component = {RegisterStudent}/>
+        <Route path = '/registertutor' component = {RegisterTutor}/>
+        <Route path = '/home' component = {Home}/>
+        <Route path = '/about' component = {About}/>
+        <Route path = '/contact' component = {Contact}/>
+        <Route path = '/subjects' component = {Subjects}/>
+        <Route path = '/tutors/:subjectid' component = {TutorList}/>
+        <Route path = '/tutors/:tutorid' component = {TutorView}/>
+        <Route path = '/studentprofile/:studentid' component = {StudentProfile}/>
+        <Route path = '/tutorprofile/:tutorid' component = {TutorProfile}/>
+        <Route path = '/studentlist/:tutorid' component = {StudentList}/>
+        <Route path = '/tutorlist/:studentid' component = {MyTutors}/>
+        {/* <Route path = '/tutorlist/:tutorid' component = {TutorList}/> */}
+    </Switch>
+)
