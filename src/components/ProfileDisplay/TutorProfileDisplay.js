b067cc5411852f1798d543655c5c77556469ec8d
Matthew Bodily <matthewbodily@Matthews-MacBook-Air.local>
2019-02-28 09:24:54 -0700

    added some styling to the tutor profile component to put a border around the picture and information

--- src/components/ProfileDisplay/TutorProfileDisplay.js
+++ src/components/ProfileDisplay/TutorProfileDisplay.js
@@ -13,7 +13,7 @@ const TutorProfileDisplay = props => {
                         src = {props.tutor.profile_pic} 
                         alt = 'Profile Pic'/>
                 </div>
-                <div>
+                <div className = 'Infobox'>
                     <p>{props.tutor.first_name} {props.tutor.last_name}</p>
                     <p>{props.tutor.email}</p>
                     <p>${props.tutor.price}</p>
@@ -22,10 +22,12 @@ const TutorProfileDisplay = props => {
             </div>) : (
             <div>
                 <Link to = '/uploadtutorphoto'><button>Add a Photo</button></Link>
-                <p>{props.tutor.first_name} {props.tutor.last_name}</p>
-                <p>{props.tutor.email}</p>
-                <p>${props.tutor.price}</p>
-                <p>{props.tutor.tutor_description}</p>
+                <div className = 'Infobox'>
+                    <p>{props.tutor.first_name} {props.tutor.last_name}</p>
+                    <p>{props.tutor.email}</p>
+                    <p>${props.tutor.price}</p>
+                    <p>{props.tutor.tutor_description}</p>
+                </div>
             </div>
             )
         }
